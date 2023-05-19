21b96f3abc8f3314b6ac347e15929b20dceb676a
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-19 18:38:52 +0100

    add new methods

--- lib/pizzeria.rb
+++ lib/pizzeria.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Pizzeria
   attr_reader :place_name, :menu, :order, :customer
 
@@ -6,24 +8,36 @@ class Pizzeria
     @menu = Menu.new
     @order = Order.new
     @customer = nil
-    #configure_twilio
+    configure_twilio
   end
 
   def display_menu
     @menu.display_menu
   end
 
-  def place_order(customer, *selection)
-    # Place an order by adding selected dishes to the order object
-    # Returns nothing
+  def process_order(customer, *selection)
+    selection.each do |dish|
+      if menu.available?(dish)
+        order.add_item(dish)
+        puts "Added #{dish} to the order."
+      else
+        puts "Sorry, #{dish} is not available."
+      end
+    end
+    send_order_confirmation(customer)
   end
 
   def print_receipt
     @order.print_receipt(@menu)
   end
 
-  def send_confirmation_sms
-    # Send a confirmation SMS to the customer using the Twilio API
-    # Returns nothing
+  private 
+
+  def configure_twilio
+    Twilio.configure do |config|
+      config.account_sid = ENV['TWILIO_ACCOUNT_SID']
+      config.auth_token = ENV['TWILIO_AUTH_TOKEN']
+    end
   end
 end
+
