6008c8d52253ba395a020ef76e8c6658f601d56a
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-19 16:28:24 +0100

    add tests, pass tests

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,12 +1,33 @@
 require 'customer'
 
 RSpec.describe Customer do
-  let(:customer_1) {Customer.new("Eugene", "West Ham, 18", "0175674353")}
+  let(:menu) { instance_double('Menu') }
+  let(:order) { instance_double('Order') }
+  let(:pizzeria) { double('Pizzeria', menu: menu, order: order, display_menu: nil, place_name: 'Makers Pizza') }
+  subject(:customer) { described_class.new('John Hannis', '123 Main St', '0178758659') }
+
   context "isolated test" do
-    it "returns name, address and phone number" do
-      expect(customer_1.name).to eq "Eugene"
-      expect(customer_1.address).to eq "West Ham, 18"
-      expect(customer_1.phone_number).to eq "0175674353"
+    it "returns name, address, and phone number" do
+      expect(customer.name).to eq "John Hannis"
+      expect(customer.address).to eq "123 Main St"
+      expect(customer.phone_number).to eq "0178758659"
+    end
+  end
+
+  context "#view_menu using double" do
+    it "calls the display_menu method on the pizzeria" do
+      expect(pizzeria).to receive(:display_menu)
+      customer.view_menu(pizzeria)
+    end
+  end
+
+  context '#view_receipt method' do
+    it 'calls the print_receipt method on the pizzeria' do
+      expect(order).to receive(:print_receipt).with(menu)
+      customer.view_receipt(pizzeria)
     end
   end
 end
+
+
+
