0ef1cf401c1f3f73f05ea71ee56438c91084a92b
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-21 16:02:45 +0100

    refactor

--- spec/pizzeria_spec.rb
+++ spec/pizzeria_spec.rb
@@ -4,17 +4,13 @@ require 'twilio-ruby'
 RSpec.describe Pizzeria do
   let(:menu_double) { instance_double(Menu) }
   let(:order_double) { instance_double(Order) }
+  let(:customer) { instance_double(Customer) }
   let(:customer_double) { instance_double(Customer) }
   let(:pizzeria) { Pizzeria.new }
 
-  before do
-    allow(order_double).to receive(:items).and_return([])
-    allow(Menu).to receive(:new).and_return(menu_double)
-    allow(Order).to receive(:new).and_return(order_double)
-  end
-
   describe "#display_menu" do
     it "calls the display_menu method of the menu" do
+      allow(Menu).to receive(:new).and_return(menu_double)
       expect(menu_double).to receive(:display_menu)
       pizzeria.display_menu
     end
@@ -23,6 +19,10 @@ RSpec.describe Pizzeria do
   describe "#process_order" do
     context "when dishes are available" do
       it "adds selected dishes to the order" do
+        allow(Menu).to receive(:new).and_return(menu_double)
+        allow(order_double).to receive(:items).and_return([])
+        allow(Order).to receive(:new).and_return(order_double)
+
         selection = ["Margherita", "Hawaiian"]
 
         allow(menu_double).to receive(:available?).and_return(true)
